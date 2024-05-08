3f9e80a76b17801e7213cfd617d8c87ec415f329
ByteMeDirk <bytemedirk@proton.me>
2024-05-08 10:30:05 +0200

    Using Postgres

--- paperapp/paperapp/settings.py
+++ paperapp/paperapp/settings.py
@@ -81,9 +81,13 @@ WSGI_APPLICATION = "paperapp.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
