7c0c4c5c38a3d875ebbb757a5f796fe58923b2b6
ByteMeDirk <bytemedirk@proton.me>
2024-05-09 09:32:47 +0200

    Cleaned up home pager and searchbars

---
+++ paperapp/paperapp/utils.py
@@ -0,0 +1,35 @@
+from django.core.paginator import Paginator
+
+from multimedia.models import ImagePost, VideoPost, AudioPost
+
+
+def get_media_pagination(request, page_limit:int, order_by:str = "-created_at"):
+    """
+    This function returns the paginated media objects.
+
+    Args:
+        request (HttpRequest): The request object.
+        page_limit (int): The number of media objects per page.
+        order_by (str): The field to order the media objects by.
+
+    Returns:
+        image_page_obj (Paginator): The paginated image objects.
+        video_page_obj (Paginator): The paginated video objects.
+        audio_page_obj (Paginator): The paginated audio objects.
+    """
+    # Get all media static
+    image_media = ImagePost.objects.all().order_by(order_by)
+    video_media = VideoPost.objects.all().order_by(order_by)
+    audio_media = AudioPost.objects.all().order_by(order_by)
+
+    # Define Media Pagination
+    image_paginator = Paginator(image_media, page_limit)
+    video_paginator = Paginator(video_media, page_limit)
+    audio_paginator = Paginator(audio_media, page_limit)
+
+    # Get the page
+    image_page_obj = image_paginator.get_page(request.GET.get("image_page"))
+    video_page_obj = video_paginator.get_page(request.GET.get("video_page"))
+    audio_page_obj = audio_paginator.get_page(request.GET.get("audio_page"))
+
+    return image_page_obj, video_page_obj, audio_page_obj
--- paperapp/paperapp/views.py
+++ paperapp/paperapp/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from multimedia.models import ImagePost, VideoPost, AudioPost
+from paperapp.utils import get_media_pagination
 
 
 @login_required(login_url="about")
@@ -10,31 +11,13 @@ def home(request):
     """
     View for the home page.
     """
-    # Get all media static
-    image_media = ImagePost.objects.all().order_by("-created_at")
-    video_media = VideoPost.objects.all().order_by("-created_at")
-    audio_media = AudioPost.objects.all().order_by("-created_at")
-
-    # Define Media Pagination
-    image_paginator = Paginator(image_media, 4)
-    video_paginator = Paginator(video_media, 4)
-    audio_paginator = Paginator(audio_media, 4)
-
-    # Get the page number
-    image_page_number = request.GET.get("image_page")
-    video_page_number = request.GET.get("video_page")
-    audio_page_number = request.GET.get("audio_page")
-
-    # Get the page
-    image_page_obj = image_paginator.get_page(image_page_number)
-    video_page_obj = video_paginator.get_page(video_page_number)
-    audio_page_obj = audio_paginator.get_page(audio_page_number)
+    # Get the pages
+    image_page_obj, video_page_obj, audio_page_obj = get_media_pagination(
+        request, 4, "-created_at"
+    )
 
     # Other Stats to be added
     recent_20_tags = ImagePost.tags.most_common()[:20]
-    total_image_posts = ImagePost.objects.count()
-    total_video_posts = VideoPost.objects.count()
-    total_audio_posts = AudioPost.objects.count()
 
     return render(
         request,
@@ -44,11 +27,6 @@ def home(request):
             "video_page_obj": video_page_obj,
             "audio_page_obj": audio_page_obj,
             "recent_20_tags": recent_20_tags,
-            "media_count": {
-                "image": total_image_posts,
-                "video": total_video_posts,
-                "audio": total_audio_posts,
-            },
         },
     )
 
