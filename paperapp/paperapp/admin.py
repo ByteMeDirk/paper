8df0a2b17b71795c68b8191512afce74266d9478
ByteMeDirk <bytemedirk@proton.me>
2024-05-10 22:07:56 +0200

    Updated admin

--- paperapp/paperapp/admin.py
+++ paperapp/paperapp/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
+from users.models import Profile
 from multimedia.models import (
     ImagePost,
     VideoPost,
     AudioPost,
     MediaRating,
 )
-from users.models import Profile
-
+from moderation.models import ReportUser, ReportPost
+admin.site.register(Profile)
 admin.site.register(ImagePost)
 admin.site.register(VideoPost)
 admin.site.register(AudioPost)
 admin.site.register(MediaRating)
-admin.site.register(Profile)
+admin.site.register(ReportUser)
+admin.site.register(ReportPost)
+
+
